1978b189c3192320953d2d3c7d7be36e74cd5ec1
Andres Rodriguez <andresfrl02@hotmail.com>
2026-03-12 22:07:05 -0500

    vercel.json actualizado

---
+++ marketplace-api/api/index.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify, request
+import random
+
+app = Flask(__name__)
+
+
+@app.route('/v1/marketplace', methods=['GET'])
+def marketplace():
+    ingrediente = request.args.get('ingredient')
+    if not ingrediente:
+        return jsonify({"error": "Falta el parámetro ingredient"}), 400
+
+    cantidad = random.randint(1, 10)
+    return jsonify({"data": {ingrediente: cantidad}})
